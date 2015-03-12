4bd0ddf489bb65aa475839d3a0de390d2351042a
Yanxing Wang <demiwangya@gmail.com>
2015-03-12 15:33:30 -0700

    new benchmark (vector::normalize)

--- benchmark/vec3_norm/benchmark.js
+++ benchmark/vec3_norm/benchmark.js
@@ -1,36 +1,30 @@
-var Vector = (function () {
-    Vector.prototype.length = function () {
-        return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
-    }
-    Vector.prototype.divideScalar = function( scalar ) {
-        if ( scalar !== 0 ) {
-            var invScalar = 1 / scalar;
-            this.x *= invScalar;
-            this.y *= invScalar;
-            this.z *= invScalar;
-        } else {
-            this.x = 0;
-            this.y = 0;
-            this.z = 0;
-        }
-        return this;
-    }
-    Vector.prototype.normalize = function () {
-        return this.divideScalar( this.length() );
-    }
-    function Vector(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }    
-    return Vector;
-})();
+function Vector() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+}
+Vector.prototype.length = function () {
+    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
+Vector.prototype.divideScalar = function( scalar ) {
+    var invScalar = 1 / scalar;
+    this.x *= invScalar;
+    this.y *= invScalar;
+    this.z *= invScalar;
+    return this;
+}
+Vector.prototype.normalize = function () {
+    return this.divideScalar( this.length() );
+}
 
 
 var start = new Date().getTime();
 
 for(var i = 0; i < 1000000; ++i) {
-    var v = new Vector(i,i/2.0,i/3.0);
+    var v = new Vector();
+    v.x = i;
+    v.y = i/2.0;
+    v.z = i/3.0;
     v.length();
 }
 
--- benchmark/vec3_norm/benchmark_to.js
+++ benchmark/vec3_norm/benchmark_to.js
@@ -1,43 +1,30 @@
-var Vector = (function () {
-    var _Vector = new TypedObject.StructType({
-        x: TypedObject.float64,
-        y: TypedObject.float64,
-        z: TypedObject.float64,
-    });
-    _Vector.prototype.length = function () {
-        return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
-    }
-    _Vector.prototype.divideScalar = function( scalar ) {
-        if ( scalar !== 0 ) {
-            var invScalar = 1 / scalar;
-            this.x *= invScalar;
-            this.y *= invScalar;
-            this.z *= invScalar;
-        } else {
-            this.x = 0;
-            this.y = 0;
-            this.z = 0;
-        }
-        return this;
-    }
-    _Vector.prototype.normalize = function () {
-        return this.divideScalar( this.length() );
-    }
-    function Vector(x, y, z) {
-        var obj = new _Vector();
-        obj.x = x;
-        obj.y = y;
-        obj.z = z;
-        return obj;
-    }    
-    return Vector;
-})();
+var Vector = new TypedObject.StructType({
+    x: TypedObject.float64,
+    y: TypedObject.float64,
+    z: TypedObject.float64,
+});
+Vector.prototype.length = function () {
+    return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
+Vector.prototype.divideScalar = function( scalar ) {
+    var invScalar = 1 / scalar;
+    this.x *= invScalar;
+    this.y *= invScalar;
+    this.z *= invScalar;
+    return this;
+}
+Vector.prototype.normalize = function () {
+    return this.divideScalar( this.length() );
+}
 
 
 var start = new Date().getTime();
 
 for(var i = 0; i < 1000000; ++i) {
-    var v = new Vector(i,i/2.0,i/3.0);
+    var v = new Vector();
+    v.x = i;
+    v.y = i/2.0;
+    v.z = i/3.0;
     v.length();
 }
 
